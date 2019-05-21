1dfec1cf1c96e16ef2da3986576e12f291c5d54b
Przemysław K. Rekucki <przemyslaw.rekucki@golem.network>
2019-05-21 19:05:38 +0200

    command json mock

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
+use actix_web::{web, App, HttpServer, Responder, post, HttpResponse};
+use futures::{future, prelude::*};
+
+mod command;
+
+#[post("/api")]
+fn api(body : web::Json<command::Command>) -> impl Future<Item=HttpResponse, Error=actix_web::error::Error> {
+    eprintln!("command={:?}", body.0);
+    match body.0 {
+        command::Command::Id => {
+            let id = "123".into();
+            let version = "0.3.0".into();
+
+            future::ok(HttpResponse::Ok().json(command::IdResult {
+                id, version
+            }))
+        }
+        command::Command::Addresses => {
+            future::ok(HttpResponse::Ok().json(command::AddressesResult {
+                addresses: command::AddressSpec::TCP {
+                    address: "0.0.0.0".into(),
+                    port: 3282
+                }
+            }))
+        }
+        _ => unimplemented!()
+    }
+}
 
 
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(api))
+        .bind("127.0.0.1:3292")?
+        .run()
 }
