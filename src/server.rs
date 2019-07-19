5983bdaeb84a4d2dd484c062c415f83c308e485f
Przemysław Rekucki <przemyslaw.rekucki@golem.network>
2019-07-19 16:11:36 +0200

    multicore server for transfer protocol. (#13)

--- src/server.rs
+++ src/server.rs
@@ -1,45 +1,25 @@
 use crate::database::DatabaseManager;
 use actix::prelude::*;
+use actix_server::Io;
+use actix_service::service_fn;
 
-use std::net;
-use tokio_tcp::{TcpListener, TcpStream};
+use std::{io, net};
+use tokio_tcp::TcpStream;
 
-pub struct Server {
+pub fn new(
     db: Addr<DatabaseManager>,
-}
-
-impl Server {
-    pub fn new(db: Addr<DatabaseManager>, listener: TcpListener) -> Addr<Self> {
-        Self::create(|ctx| {
-            ctx.add_message_stream(listener.incoming().map_err(|_| ()).and_then(|st| {
-                let addr = st
-                    .peer_addr()
-                    .map_err(|e| log::error!("get peer addr: {}", e))?;
-                Ok(TcpConnect(st, addr))
-            }));
-            Server { db }
-        })
-    }
-}
-
-#[derive(Message)]
-pub struct TcpConnect(pub TcpStream, pub net::SocketAddr);
-
-impl Actor for Server {
-    type Context = Context<Self>;
-
-    fn started(&mut self, _ctx: &mut Self::Context) {
-        // TODO
-    }
-}
-
-impl Handler<TcpConnect> for Server {
-    /// this is response for message, which is defined by `ResponseType` trait
-    /// in this case we just return unit.
-    type Result = ();
-
-    fn handle(&mut self, msg: TcpConnect, _: &mut Context<Self>) {
-        log::info!("Connection from: {}", msg.1);
-        let _conn = crate::connection::Connection::new(self.db.clone(), msg.0, msg.1);
-    }
+    addr: impl net::ToSocketAddrs,
+) -> io::Result<actix_server::Server> {
+    Ok(actix_server::Server::build()
+        .bind("gst", addr, move || {
+            let db = db.clone();
+            service_fn(move |stream: Io<TcpStream>| {
+                let (tcp_stream, (), _) = stream.into_parts();
+                let peer_addr = tcp_stream.peer_addr()?;
+                log::info!("Connection from: {}", peer_addr);
+                let _conn = crate::connection::Connection::new(db.clone(), tcp_stream, peer_addr);
+                Ok::<_, io::Error>(())
+            })
+        })?
+        .start())
 }
